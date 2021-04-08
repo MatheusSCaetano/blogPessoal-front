7d943f56c41190a62b74a8a50e192f60cc5719a9
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-04-08 14:00:26 -0300

    PUT de Postagens

--- Front-end/blogPessoal/src/app/service/postagem.service.ts
+++ Front-end/blogPessoal/src/app/service/postagem.service.ts
@@ -26,5 +26,17 @@ postPostagem(postagem: Postagem): Observable<Postagem>{
   return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
 }
 
+putPostagem(postagem: Postagem): Observable<Postagem>{
+  return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+}
+
+getByIdPostagem(id: number): Observable<Postagem>{
+  return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`,this.token)
+}
+
+deletePostagem(id: number){
+  return this.http.delete(`http://localhost:8080/postagens/${id}`,this.token)
+}
+
 }
 
